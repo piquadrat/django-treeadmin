7c4b881cec29bfbfe5f6d00c7aed3bd20ca82513
Guglielmo <guglielmo.celata@gmail.com>
2014-04-24 19:57:34 +0200

    simplejson/json import corrected

--- treeadmin/admin.py
+++ treeadmin/admin.py
@@ -320,7 +320,7 @@ class TreeAdmin(admin.ModelAdmin):
                 d.append(b)
 
         # TODO: Shorter: [ y for x,y in zip(a,b) if x!=y ]
-        return HttpResponse(simplejson.dumps(d), mimetype="application/json")
+        return HttpResponse(json.dumps(d), mimetype="application/json")
 
     def get_changelist(self, request, **kwargs):
         return ChangeList
@@ -349,7 +349,7 @@ class TreeAdmin(admin.ModelAdmin):
         extra_context = extra_context or {}
         extra_context['TREEADMIN_MEDIA_HOTLINKING'] = self.jquery_use_google_cdn
         extra_context['TREEADMIN_JQUERY_NO_CONFLICT'] = self.jquery_no_conflict
-        extra_context['tree_structure'] = mark_safe(simplejson.dumps(
+        extra_context['tree_structure'] = mark_safe(json.dumps(
             _build_tree_structure(self.model)))
 
         return super(TreeAdmin, self).changelist_view(request, extra_context, *args, **kwargs)
